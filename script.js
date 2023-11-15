37781a040cc685ff8eadf4698cdea6712535dde6
vamshibittu <143110438+vamshibittu@users.noreply.github.com>
2023-11-15 14:38:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
